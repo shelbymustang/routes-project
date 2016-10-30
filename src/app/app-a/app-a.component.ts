7437b35b2d5f9c5d69c23bac98b185cba6c0cb34
Jim Shelby <jim-shelby@gorvw.net>
2016-10-30 13:42:20 -0700

    added test components

---
+++ src/app/app-a/app-a.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rp-app-a',
+  template: `
+    
+      <h1> Component A</h1>
+    
+  `,
+  styles: []
+})
+export class AppAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/app-b/app-b.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'rp-app-b',
+  template: `
+    
+      <h1>Component B - Value {{id}}</h1>
+        <button (click)="onNavigate()">Goto Comp A</button>
+  `,
+  styles: []
+})
+export class AppBComponent implements OnDestroy {
+  private subscription: Subscription;
+  id:string;
+
+  onNavigate() {
+    this.router.navigate(['/']);
+
+  }
+
+  ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
+//  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+//    this.id = activatedRoute.snapshot.params['id']; }
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.subscription = activatedRoute.params.subscribe(
+        (param:any) => this.id = param['id']
+        );
+  }
+
+
+}
